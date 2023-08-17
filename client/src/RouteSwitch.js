78f322b3904643dae51be3844964d4437d969ad7
djmginger <djmginger@gmail.com>
2023-08-17 18:37:38 -0400

    Enabled Google analytics collection, and bugfixed navbar brand

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -6,8 +6,16 @@ import Characters from "./pages/Characters";
 import CharacterDisplay from "./pages/CharacterDisplay";
 import Protected from "./components/Protected";
 import Profile from "./pages/Profile";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-GJYDY72B3N');
 
 const RouteSwitch = () => {
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
